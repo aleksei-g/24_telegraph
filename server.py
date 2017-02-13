e832e84428e4da4bd7a5eb04a5cde98864ddb008
aleksei-g <aleksei_g@outlook.com>
2017-02-13 15:23:27 +0300

    POST AJAX

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, json
+from flask import Flask, render_template, request, json, make_response
 from os import urandom
 import binascii
+from config import site_title
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,29 +11,37 @@ from models import db, Articles
 db.create_all()
 
 
+def get_article_data(article):
+    return {'title': site_title,
+            'article_id': article.id,
+            'header': article.header,
+            'signature': article.signature,
+            'body': article.body,
+            'author': request.cookies.get('user_id') == article.user_id,
+            }
+
+
 @app.route('/')
 def form():
-    return render_template('form.html')
+    user_id = request.cookies.get('user_id')
+    if not user_id:
+        user_id = str(binascii.hexlify(urandom(24)))
+    resp = make_response(render_template('telegraph.html', title=site_title))
+    resp.set_cookie('user_id', user_id, max_age=604800)
+    return resp
 
 
 @app.route('/<int:article_id>')
 def show_article(article_id):
     article = Articles.query.get_or_404(article_id)
-    article_data = {'header': article.header,
-                    'signature': article.signature,
-                    'body': article.body,
-                    'article_id': article_id,
-                    'edit': request.cookies.get('user_id') == article.user_id,
-                    }
-    return render_template('article.html', **article_data)
+    article_data = get_article_data(article)
+    return render_template('telegraph.html', **article_data)
 
 
 @app.route('/save/', methods=['POST'])
 @app.route('/save/<int:article_id>', methods=['POST'])
 def save_article(article_id=None):
     user_id = request.cookies.get('user_id')
-    if not user_id:
-        user_id = str(binascii.hexlify(urandom(24)))
     if article_id:
         article = Articles.query.get_or_404(article_id)
         article.header = request.form['header']
@@ -46,24 +55,8 @@ def save_article(article_id=None):
                            )
     db.session.add(article)
     db.session.commit()
-    return json.dumps({'status': 'ok',
-                       'path': article.id,
-                       'cookie_str': 'user_id={}; max-age={}'.format(user_id,
-                                                                     604800)
-                       })
-
-
-@app.route('/edit/<int:article_id>')
-def edit_article(article_id):
-    article = Articles.query.get_or_404(article_id)
-    if request.cookies.get('user_id') != article.user_id:
-        return redirect(url_for('.form'))
-    article_data = {'header': article.header,
-                    'signature': article.signature,
-                    'body': article.body,
-                    'article_id': article_id,
-                    }
-    return render_template('form.html', **article_data)
+    article_data = get_article_data(article)
+    return json.dumps(article_data)
 
 
 if __name__ == "__main__":
--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,47 @@
-function SendForm(){
+$(function(){
+  if (window.location.pathname == '/') {
+    EditArticle()
+  }
+  else {
+    ShowArticle()
+  };
+});
+
+$("#postForm").submit(function(event) {
+  event.preventDefault();
   $.ajax({
-    url: '/save/'+articleID,
+    url: '/save/'+$('form').find( 'input[name="author_id"]' ).val( ),
     type: 'POST',
     dataType: 'json',
     data: $('form').serialize(),
-    success: function(response){
-      document.cookie = response.cookie_str;
-      window.location.pathname = response.path;
+    success: function(response) {
+      history.pushState(null, null, response.article_id);
+      document.title = response.header + ' / '+ response.title;
+      $('form').find( 'input[name="author_id"]' ).val( response.article_id );
+      $("#postForm").hide();
+      $("#result").show();
+      $( "div#result .header" ).html( response.header );
+      $( "div#result .signature" ).html( response.signature );
+      $( "div#result .article" ).html( response.body );
+      if (response.author == true) {
+        $("div#result .edit-btn").show();
+      };
     },
   });
+});
+
+function EditArticle(){
+  $("#postForm").show();
+  $("#result").hide();
+};
+
+function ShowArticle(){
+  $("#postForm").hide();
+  $("#result").show();
+  if ( author == "True" ) {
+    $("div#result .edit-btn").show();
+  }
+  else {
+    $("div#result .edit-btn").hide();
+  };
 };
